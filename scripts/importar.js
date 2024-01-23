635e96dabf5df26150e4a53fa7e7111e70893aae
RantuchoM <martin.rantucho@gmail.com>
2024-01-23 09:16:39 -0300

    favicon

--- scripts/importar.js
+++ scripts/importar.js
@@ -1,6 +1,9 @@
 document.write('<script src="https://code.jquery.com/jquery-3.5.1.js"></script>');
 document.write('<script src="https://apis.google.com/js/api.js"></script>');
 
+document.write('<link rel="shortcut icon" href="images/favicon.ico" type="images/x-icon">');
+document.write('<link rel="icon" href="images/favicon.ico" type="images/x-icon">');
+
 document.write('<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet" />');
 document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>');
 document.write('<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.0.272/jspdf.debug.js"></script>');
